db4113322abbdbea6ae5c5060819958cdc624e09
weichichou <weichi.cc@gmail.com>
2020-02-13 22:27:29 +0100

    Finish the form

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -15,14 +15,29 @@ export default class SignUp extends Component {
   constructor(props) {
     super(props);
 
+    this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = { newUser: null };
+  }
+  componentDidMount() {
+    // focus on the "name" field
+    this.refs.form.getComponent("name").refs.input.focus();
   }
 
+  clearForm() {
+    this.setState({ newUser: null });
+  }
+
+  onChange(newUser) {
+    this.setState({ newUser });
+  }
   onSubmit() {
     const { form } = this.refs;
     const newUser = form.getValue();
     if (!newUser) return;
     console.log(newUser);
+    this.clearForm();
   }
 
   render() {
@@ -32,7 +47,13 @@ export default class SignUp extends Component {
       <View style={styles.outerContainer}>
         <KeyboardAvoidingView behavior="padding" style={styles.container}>
           <Text style={styles.title}>Sign up for ShatApp</Text>
-          <Form ref="form" type={Person} options={formOptions} />
+          <Form
+            ref="form"
+            type={Person}
+            options={formOptions}
+            value={this.state.newUser}
+            onChange={this.onChange}
+          />
           <TouchableHighlight
             style={styles.button}
             onPress={this.onSubmit}
