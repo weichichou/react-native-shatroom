d745c3e0cbff5520e1b1c0f6608bc9f16257e40b
weichichou <weichi.cc@gmail.com>
2020-02-13 22:24:17 +0100

    Add KeyboardAvoidingView

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { View, Text, TouchableHighlight } from "react-native";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableHighlight
+} from "react-native";
 
 import styles from "./SignUp.styles";
 
@@ -7,20 +12,35 @@ import t from "tcomb-form-native";
 import Person, { formOptions } from "../models/Person";
 
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+    const { form } = this.refs;
+    const newUser = form.getValue();
+    if (!newUser) return;
+    console.log(newUser);
+  }
+
   render() {
     const Form = t.form.Form;
 
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Sign up for ShatApp</Text>
-        <Form ref="form" type={Person} options={formOptions} />
-        <TouchableHighlight
-          style={styles.button}
-          onPress={this.onSubmit}
-          underlayColor="#99d9f4"
-        >
-          <Text style={styles.buttonText}>Sign up</Text>
-        </TouchableHighlight>
+      <View style={styles.outerContainer}>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <Text style={styles.title}>Sign up for ShatApp</Text>
+          <Form ref="form" type={Person} options={formOptions} />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.onSubmit}
+            underlayColor="#99d9f4"
+          >
+            <Text style={styles.buttonText}>Sign up</Text>
+          </TouchableHighlight>
+        </KeyboardAvoidingView>
       </View>
     );
   }
--- screens/SignUp.styles.js
+++ screens/SignUp.styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  outerContainer: {
+    flex: 1
+  },
   container: {
     flex: 1,
     justifyContent: "center",
