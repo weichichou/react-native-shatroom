aa1c7f145c5442197d1b3b51f23ed8a005e81ad2
weichichou <weichi.cc@gmail.com>
2020-02-13 21:58:53 +0100

    Add app.styles

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+//import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import SignUp from "./screens/SignUp";
+import styles from "./App.styles";
 
 export default class App extends React.Component {
   render() {
-    return <SignUp />;
+    return (
+      <View style={styles.container}>
+        <SignUp />
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
   }
-});
+}); */
